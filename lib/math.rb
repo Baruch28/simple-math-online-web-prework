4789ce69ed7073a4e3331e839c9a1d4548425abf
mark baruch <mbaruch28@gmail.com>
2019-02-01 17:52:15 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,28 @@
 def addition(num1, num2)
+  2 + 9 = 11
 end
 
 def subtraction(num1, num2)
+  55 - 32 = 23
 end
 
 def division(num1, num2)
+  42.0 / 6 = 7
 end
 
 def multiplication(num1, num2)
+  5 * 2 = 10
 end
 
 def modulo(num1, num2)
+  9 % 2
 end
 
 def square_root(num)
+  Math.sqrt(64)
 end
 
 def order_of_operation(num1, num2, num3, num4)
+  8 + (( 4 * 4 ) / 5 )
   #Hint:  __ + (( __ * __ ) / __ )
 end
